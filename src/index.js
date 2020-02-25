6d9e3fd07fa222e39959e1e4ba60719d8c27e1c6
nwindian <jmc361@zips.uakron.edu>
2020-02-25 14:29:36 -0500

    Create entry page taskbar

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
+
+class HomeTaskBar extends React.Component{
+
+    render() {
+        return (
+            <div class = "topnav">
+                  <a href="#SignUp">SignUp</a>
+                  <a href="#Login">Login</a>
+            </div>
+        );
+    }
+}
+class Entry extends React.Component {
+
+    render() {
+        return (
+            <div className="HomeTaskBar">
+                <HomeTaskBar />
+            </div>   
+        );
+    }
+}
+
+ReactDOM.render(
+    <Entry />,
+    document.getElementById('root')
+);
